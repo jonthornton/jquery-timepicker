f1873b5bfb2a8af0934ccffbff900ffa618d867a
Phil Freo <phil@philfreo.com>
2013-01-16 17:34:51 -0800

    Fix offset position when using appendTo

--- jquery.timepicker.js
+++ jquery.timepicker.js
@@ -131,12 +131,20 @@ requires jQuery 1.7+
 			// make sure other pickers are hidden
 			methods.hide();
 
+			var settings = self.data('timepicker-settings');
+
+			settings.appendTo = $(settings.appendTo);
+			var parentOffset = {
+				top: settings.appendTo.offset().top - settings.appendTo.scrollTop(),
+				left: settings.appendTo.offset().left - settings.appendTo.scrollLeft(),
+			};
+
 			if ((self.offset().top + self.outerHeight(true) + list.outerHeight()) > $(window).height() + $(window).scrollTop()) {
 				// position the dropdown on top
-				list.css({ 'left':(self.offset().left), 'top': self.offset().top - list.outerHeight() });
+				list.css({ 'left':(self.offset().left - parentOffset.left), 'top': self.offset().top - list.outerHeight() - parentOffset.top });
 			} else {
 				// put it under the input
-				list.css({ 'left':(self.offset().left), 'top': self.offset().top + self.outerHeight() });
+				list.css({ 'left':(self.offset().left - parentOffset.left), 'top': self.offset().top + self.outerHeight() - parentOffset.top });
 			}
 
 			list.show();
