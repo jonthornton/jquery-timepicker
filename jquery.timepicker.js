da46027a52382b8a75dc7657d5d87d98953e298c
David Allouche <david@allouche.net>
2013-03-05 18:49:26 +0100

    Fix menu positioning when options.appendTo is a positioned element.

--- jquery.timepicker.js
+++ jquery.timepicker.js
@@ -132,15 +132,15 @@ requires jQuery 1.7+
 			// make sure other pickers are hidden
 			methods.hide();
 
+			list.show();
+
 			if ((self.offset().top + self.outerHeight(true) + list.outerHeight()) > $(window).height() + $(window).scrollTop()) {
 				// position the dropdown on top
-				list.css({ 'left':(self.offset().left), 'top': self.offset().top - list.outerHeight() });
+				list.offset({ 'left':(self.offset().left), 'top': self.offset().top - list.outerHeight() });
 			} else {
 				// put it under the input
-				list.css({ 'left':(self.offset().left), 'top': self.offset().top + self.outerHeight() });
+				list.offset({ 'left':(self.offset().left), 'top': self.offset().top + self.outerHeight() });
 			}
-
-			list.show();
 
 			// position scrolling
 			var selected = list.find('.ui-timepicker-selected');
