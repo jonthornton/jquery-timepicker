57197bbd494afded0e80af78ec38bb25c4ac2c7d
Jon Thornton <thornton.jon@gmail.com>
2014-06-19 22:12:16 -0400

    Tweaked build settings

--- GruntFile.js
+++ GruntFile.js
@@ -2,7 +2,18 @@ module.exports = function(grunt) {
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		meta: {
+			banner : '/*!\n' +
+			' * <%= pkg.title %> v<%= pkg.version %> - <%= pkg.description %>\n' +
+			' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %> - <%= pkg.homepage %>\n' +
+			' * License: <%= pkg.license %>\n' +
+			' */\n\n'
+		},
 		uglify: {
+			options : {
+				banner : '<%= meta.banner %>',
+				report: 'gzip'
+			},
 			dist: {
 				files: {
 					'jquery.timepicker.min.js': ['jquery.timepicker.js']
