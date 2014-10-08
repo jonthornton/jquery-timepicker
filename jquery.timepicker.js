f4a33d025037900ea0f38924b2c8d981927acaec
Gady Pitaru <gady@badgermapping.com>
2014-10-08 13:44:11 -0700

    return selected time value to selectTime event callback

--- jquery.timepicker.js
+++ jquery.timepicker.js
@@ -759,7 +759,7 @@ requires jQuery 1.7+
 		if (self.data('ui-timepicker-value') != value) {
 			self.data('ui-timepicker-value', value);
 			if (source == 'select') {
-				self.trigger('selectTime').trigger('changeTime').trigger('change', 'timepicker');
+				self.trigger('selectTime', [value]).trigger('changeTime').trigger('change', 'timepicker');
 			} else if (source != 'error') {
 				self.trigger('changeTime');
 			}
