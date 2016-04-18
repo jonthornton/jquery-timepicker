a2f08634aa23292ad44660299e2cc21f21d7ff5c
Jon Thornton <thornton.jon@gmail.com>
2016-04-17 20:59:30 -0400

    Fix range checking bug
    Close #506

--- jquery.timepicker.js
+++ jquery.timepicker.js
@@ -726,9 +726,8 @@
 
 		var rangeError = false;
 		// check that the time in within bounds
-		if (settings.minTime !== null && seconds < settings.minTime) {
-			rangeError = true;
-		} else if (settings.maxTime !== null && seconds > settings.maxTime) {
+		if (settings.minTime !== null && seconds < settings.minTime
+			&& settings.maxTime !== null && seconds > settings.maxTime) {
 			rangeError = true;
 		}
 
