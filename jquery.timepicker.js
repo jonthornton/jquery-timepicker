e9b351e634b71f9d957857844c0524afa4fca530
pstephane <pstephane.work@gmail.com>
2017-03-29 15:45:02 +0200

    Update jquery.timepicker.js

--- jquery.timepicker.js
+++ jquery.timepicker.js
@@ -891,6 +891,10 @@
 		var self = $(this);
 		var list = self.data('timepicker-list');
 
+		if (self.prop('readonly')) {
+		    return true;
+		}
+		
 		if (!list || !_isVisible(list)) {
 			if (e.keyCode == 40) {
 				// show the list!
