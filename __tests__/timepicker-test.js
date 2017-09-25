ac8b68a25f43b33dda39a319859d01df80faf944
Jon Thornton <thornton.jon@gmail.com>
2017-09-24 23:10:47 -0400

    Whitespace cleanup

--- __tests__/timepicker-test.js
+++ __tests__/timepicker-test.js
@@ -1,16 +1,14 @@
-jest
-  .dontMock('jquery')
-  .dontMock('../jquery.timepicker');
+jest.dontMock("jquery").dontMock("../jquery.timepicker");
 
-const $ = require('jquery');
-require('../jquery.timepicker');
+const $ = require("jquery");
+require("../jquery.timepicker");
 
-const TEST_INPUT = 'testInput'
+const TEST_INPUT = "testInput";
 
 beforeEach(() => {
-	document.body.innerHTML = `<input type="text" id="${TEST_INPUT}" />`;
+  document.body.innerHTML = `<input type="text" id="${TEST_INPUT}" />`;
 });
 
-test('timepicker initializes', () => {
-	$(`#${TEST_INPUT}`).timepicker();
+test("timepicker initializes", () => {
+  $(`#${TEST_INPUT}`).timepicker();
 });
