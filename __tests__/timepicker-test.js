dd2013a898b66688f33d52e93d9d59818e645af8
Jon Thornton <thornton.jon@gmail.com>
2017-09-24 22:54:09 -0400

    Hello world testing

---
+++ __tests__/timepicker-test.js
@@ -0,0 +1,16 @@
+jest
+  .dontMock('jquery')
+  .dontMock('../jquery.timepicker');
+
+const $ = require('jquery');
+require('../jquery.timepicker');
+
+const TEST_INPUT = 'testInput'
+
+beforeEach(() => {
+	document.body.innerHTML = `<input type="text" id="${TEST_INPUT}" />`;
+});
+
+test('timepicker initializes', () => {
+	$(`#${TEST_INPUT}`).timepicker();
+});
