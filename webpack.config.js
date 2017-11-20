6c2af647b9f41760f77ed9b87bc9b2f0d5a19912
Jon Thornton <thornton.jon@gmail.com>
2017-11-19 22:10:02 -0500

    build with webpack instead of gulp

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/jquery.timepicker.js',
+  output: {
+    filename: 'jquery.timepicker.min.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  externals: {
+    // require("jquery") is external and available
+    //  on the global var jQuery
+    "jquery": "jQuery"
+  },
+  plugins: [
+    new UglifyJSPlugin(),
+    new CopyWebpackPlugin([
+      {
+        from: 'src/static',
+        to: './'
+      }
+    ])
+  ]
+};
