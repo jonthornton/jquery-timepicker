1f7fd1293f9c3ad387f83c528f6384682090779a
Jon Thornton <thornton.jon@gmail.com>
2020-04-27 23:04:50 -0400

    Use dollar sign for jquery

--- src/__tests__/timepicker-test.js
+++ src/__tests__/timepicker-test.js
@@ -1,6 +1,6 @@
 jest.dontMock("jquery").dontMock("../jquery.timepicker");
 
-import jQuery from 'jquery';
+import $ from 'jquery';
 require('../jquery.timepicker');
 
 const TEST_INPUT = "testInput";
@@ -9,26 +9,26 @@ beforeEach(() => {
   document.body.innerHTML = `<div>
       <input type="text" id="${TEST_INPUT}" />
     </div>`;
-  jQuery(`#${TEST_INPUT}`).get(0).dataset = {}
+  $(`#${TEST_INPUT}`).get(0).dataset = {}
 });
 
 test("timepicker initializes", () => {
-  jQuery(`#${TEST_INPUT}`).timepicker();
+  $(`#${TEST_INPUT}`).timepicker();
 });
 
 test("show single string noneOption correctly", () =>{
-  jQuery(`#${TEST_INPUT}`).timepicker({
+  $(`#${TEST_INPUT}`).timepicker({
     "noneOption": "----"
   }).timepicker('show');
 
-  jQuery('.ui-timepicker-list li:first-child').trigger('click');
-  expect(jQuery(`#${TEST_INPUT}`).val()).toEqual('');
+  $('.ui-timepicker-list li:first-child').trigger('click');
+  expect($(`#${TEST_INPUT}`).val()).toEqual('');
 });
 
 test("timepicker can parse time value", () => {
-  jQuery(`#${TEST_INPUT}`).val('2:37pm');
-  jQuery(`#${TEST_INPUT}`).timepicker();
+  $(`#${TEST_INPUT}`).val('2:37pm');
+  $(`#${TEST_INPUT}`).timepicker();
 
   const expectedSecondsFromMidnight = 14 * 3600 + 37 * 60;
-  expect(jQuery(`#${TEST_INPUT}`).timepicker('getSecondsFromMidnight')).toEqual(expectedSecondsFromMidnight);
+  expect($(`#${TEST_INPUT}`).timepicker('getSecondsFromMidnight')).toEqual(expectedSecondsFromMidnight);
 });
